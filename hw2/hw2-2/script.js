b681876d7e2d49aa9a41b78683362ff19e2cb171
Alfredo Miranda <alfredo@miranda.io>
2014-11-18 00:05:04 -0600

    Sort score in ascending order instead

--- hw2/hw2-2/script.js
+++ hw2/hw2-2/script.js
@@ -1,18 +1,16 @@
 use students
 
 var cur = db.grades.find({type: 'homework'}).sort({
-  student_id: 1, score: -1
+  student_id: 1, score: 1
 });
 
-var prev = cur.next();
+var prev_student_id = -1;
 var doc;
 
 while(cur.hasNext()) {
   doc = cur.next();
-  if(prev.student_id != doc.student_id) {
-    db.grades.remove(prev);
+  if(prev_student_id != doc.student_id) {
+    db.grades.remove(doc);
+    prev_student_id = doc.student_id;
   }
-  prev = doc;
 }
-
-db.grades.remove(prev);
